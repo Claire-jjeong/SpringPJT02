125d6e9d0d28195090d3c2fcf0827bf1a30ea82c
Claire-jjeong <kjjung7026@gmail.com>
2022-01-16 23:56:11 +0900

    service class 생성

--- SpringMVC02/src/main/java/kr/smhrd/web/BoardRestController.java
+++ SpringMVC02/src/main/java/kr/smhrd/web/BoardRestController.java
@@ -8,16 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kr.board.domain.Board;
 import kr.board.mapper.BoardMapper;
+import kr.board.service.BoardService;
 
 @RestController //클라이언트에게 바로 응답해 줄 수 있는 것, Responsebody역할 함
 public class BoardRestController { //OpenAPI - JSON
 
 	@Autowired
-	private BoardMapper mapper;
+	private BoardService service;
 	
 	@RequestMapping("/boardListAjax.do")
 	public List<Board> boardListAjax() {
-		List<Board> list=mapper.boardList();
+		List<Board> list= service.boardListAjax();
 		//여기서 json data format 으로 응답 해야 함
 		// List<Board>를 -> Gson API이용 -> String(JSON) 형태로 변환
 		return list; //List<Board> -> jakson -> String(JSON)
@@ -25,19 +26,19 @@ public class BoardRestController { //OpenAPI - JSON
 	}
 	@RequestMapping("/boardInsertAjax.do")
 	public void boardInsertAjax(Board vo) {
-		mapper.boardInsert(vo);
+		service.boardInsertAjax(vo);
 	}
 	@RequestMapping("/boardContentUpdateAjax.do")
 	public void boardContentUpdateAjax(Board vo) {
 			//요청이 왔던 곳으로 다시 응답이 가야하기 때문 (jsp가 아닌 ajax로 감)
-		mapper.boardContentUpdateAjax(vo);
+		service.boardContentUpdateAjax(vo);
 	}
 	@RequestMapping("/boardDeleteAjax.do") //post:postmapping
 	public void boardDeleteAjax(int idx) {
-		mapper.boardDelete(idx);
+		service.boardDeleteAjax(idx);
 	}
 	@RequestMapping("/boardTWUpdateAjax.do")
 	public void boardTWUpdateAjax(Board vo) {
-		mapper.boardTWUpdateAjax(vo);
+		service.boardTWUpdateAjax(vo);
 	}
 }
